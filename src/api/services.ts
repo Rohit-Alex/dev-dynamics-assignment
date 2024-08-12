17c0034f95d923b84dbc8a2dbca3cd45000daccb
Rohit-Alex <rohit5012000@gmail.com>
2024-08-12 10:36:12 +0530

    feat: moved to constant

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,6 +1,18 @@
 import { UserGithubActivityResponse } from "types";
 import { GetAllResponse } from "./apiMethods";
-import { API_URL } from "./constants";
+import { API_URL, mockedData } from "./constants";
 
-export const getUserGithubActivity = (): Promise<UserGithubActivityResponse> =>
-  GetAllResponse<UserGithubActivityResponse>(API_URL.githubActivity);
+// export const getUserGithubActivity =
+//   async (): Promise<UserGithubActivityResponse> => {
+//     GetAllResponse<UserGithubActivityResponse>(API_URL.githubActivity);
+
+//   };
+
+export const getUserGithubActivity =
+  async (): Promise<UserGithubActivityResponse> => {
+    const res = await new Promise<UserGithubActivityResponse>((resolve) => {
+      setTimeout(() => resolve(mockedData as UserGithubActivityResponse), 2000);
+    });
+
+    return res;
+  };
